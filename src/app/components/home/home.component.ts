63f79b335f089e3652a1958639b7be671c7c3134
vicky125 <vigneshwrn419@gmail.com>
2019-06-01 16:54:23 +0530

    Timer

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -6,10 +6,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
-
+ 
+  time:string;
   constructor() { }
 
-  ngOnInit() {
+  ngOnInit() {    
+    this.runTime();
   }
+
+  runTime()
+  {
+    setInterval(()=>{
+      var d = new Date();
+      var h = (d.getHours()<10?'0':'') + d.getHours();
+      var m = (d.getMinutes()<10?'0':'') + d.getMinutes();
+      var s = (d.getSeconds()<10?'0':'') + d.getSeconds();
+      this.time = h+':'+m+':'+s; 
+    },1000);    
+  }
+
+
 
 }
